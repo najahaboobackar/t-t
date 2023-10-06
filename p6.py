6cec3599c0e786f077c24a186aafc048014257f1
najah <najahaboobacker79@gmail.com>
2023-10-06 23:08:55 +0530

    6

---
+++ p6.py
@@ -0,0 +1,7 @@
+n=int(input("range ....."))
+for i in range(n):
+    for j in range(i,n):
+        print("   ", end="")
+    for j in range(i+1):
+        print(" * ",end="")
+    print()            
