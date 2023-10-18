619e80748ffb7273b363d198eb0fd292c90e7fb7
najah <najahaboobacker79@gmail.com>
2023-10-18 17:23:23 +0530

    3

--- easy/elementappear1.py
+++ easy/elementappear1.py
@@ -1,12 +1,12 @@
 def main():
-    arr=[1,2,2,3,3]
+    arr=[1,1,2,2,3,3,4,5,4]
     r=unique(arr)
     print(r)
 def unique(arr):
     ans=0
     for num in arr:
         ans=ans^num
-        return ans  
+    return ans      
 main()      
             
             
