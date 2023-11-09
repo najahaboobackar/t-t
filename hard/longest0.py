5733c4094556239f8038fe5e9b8eb978d7fab197
najah <najahaboobacker79@gmail.com>
2023-11-09 16:39:16 +0530

    3

--- hard/longest0.py
+++ hard/longest0.py
@@ -1,9 +1,9 @@
-arr=[1,0,-1,6,9,0]
+arr=[9, -3, 3, -1, 6, -5]
 n=len(arr)
 maxi=0
 for i in range(n):
     sum=0
-    for j in range(n):
+    for j in range(i,n):
         sum+=arr[j]
         if sum==0:
             maxi=max(maxi,j-i+1)
