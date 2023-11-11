83d43f6fd578dc38803b77018b1473a942e72d82
najah <najahaboobacker79@gmail.com>
2023-11-11 10:42:41 +0530

    3

--- hard/countkxir.py
+++ hard/countkxir.py
@@ -1,7 +1,15 @@
 from collections import defaultdict
-arr=[4,2,2,6]
+arr=[4, 2, 2, 6, 4]
 n=len(arr)
 xr=0
+k=6
 mpp=defaultdict(int)
 mpp[xr]=1
 count=0
+for i in  range(n):
+    xr^=arr[i]
+    x=xr^k
+    count+=mpp[x]
+    mpp[xr]+=1
+print(count)    
+    
