d899f0544c86f9fbbc86f8998e455ecc58b0d5d9
najah <najahaboobacker79@gmail.com>
2023-11-15 16:43:17 +0530

    2

--- hard/mergesort.py
+++ hard/mergesort.py
@@ -1,7 +1,7 @@
 arr=[1,3,4]
-n1=len(arr)
+n1=len(arr)+1
 ar2=[2,5,6,7]
-n2=len(ar2)
+n2=len(ar2)+1
 l=[]*n1
 r=[]*n2
 n=len(max(arr,ar2))
@@ -10,17 +10,19 @@ i=0
 for i in range(n-1):
     c=ar2[i]+1 
     b=arr[i]+1
-    if arr[i]<ar2[i] and b==ar2[i]:
+    if arr[i]<ar2[i] and b==ar2[i] and i<=n1:
         l.append(arr[i])
         l.append(ar2[i])
             
-    elif ar2[i]<arr[i] and c==arr[i]:
+    elif ar2[i]<arr[i] and c==arr[i] and i<=n2:
         l.append(ar2[i])
         l.append(arr[i])    
     else:
         r.append(arr[i]) 
         r.append(ar2[i])
-        r.append(n-1)
+        r.append(ar2[n-1]) 
+       
+        
          
 l=list(set(l))
 r=list(set(r))          
