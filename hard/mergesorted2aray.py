750a999bd55a1bf5972090efc216a8bd86e6c6c5
najah <najahaboobacker79@gmail.com>
2023-11-15 17:04:02 +0530

    2

--- hard/mergesorted2aray.py
+++ hard/mergesorted2aray.py
@@ -1,14 +1,15 @@
 def main():
-    arr=[1,2,3,8]
-    arr2=[9,0,10]
+    arr=[1, 4, 8, 10]
+    arr2=[2, 3, 9]
     m=len(arr2)
     n=len(arr)
     merge(arr,arr2,n,m)
 def merge(arr,arr2,n,m):
     left=n-1 
     right=0
-    while arr[left]<arr2[right] and left>0 and right<m:
-        arr[left],arr2[right]=arr2[right],arr[left]
+    while   left>=0 and right<m:
+        if arr[left]>arr2[right]:
+            arr[left],arr2[right]=arr2[right],arr[left]
         left-=1
         right+=1
     arr2.sort()
