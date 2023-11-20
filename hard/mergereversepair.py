01aebdbee7be4fef9d8cb65ef4174ca4ccf27848
najah <najahaboobacker79@gmail.com>
2023-11-20 21:28:53 +0530

    :@:

--- hard/mergereversepair.py
+++ hard/mergereversepair.py
@@ -1,55 +1,60 @@
-def main():
-    arr= [4, 1, 2, 3, 1]
-    n=len(arr)
-    
-    r=team(arr,n-1)
-    print(r)
-    
-    
-def merge(arr,low ,high,mid):
-    temp=[]
-    left=0
-    right=mid+1
-    while left<=mid and right<=high :
-        if arr[left]<arr[right]:
+
+from typing import List
+
+def merge(arr, low, mid, high):
+    temp = []  # temporary array
+    left = low  # starting index of left half of arr
+    right = mid + 1  # starting index of right half of arr
+
+    # storing elements in the temporary array in a sorted manner
+    while left <= mid and right <= high:
+        if arr[left] <= arr[right]:
             temp.append(arr[left])
-            left+=1
-        if arr[right]<arr[left]:
-            temp.append(arr[right]) 
-            right+=1  
-    while left<=mid:
+            left += 1
+        else:
+            temp.append(arr[right])
+            right += 1
+
+    # if elements on the left half are still left
+    while left <= mid:
         temp.append(arr[left])
-        left+=1
-    while right<=high:
+        left += 1
+
+    # if elements on the right half are still left
+    while right <= high:
         temp.append(arr[right])
-        right+=1
+        right += 1
+
+    # transferring all elements from temporary to arr
     for i in range(low, high + 1):
-        arr[i] = temp[i - low]    
-def countpair(arr,low,high,mid):
-    count=0
-    right=mid+1
-    for i in range(low,mid+1):
-        while right<=high and arr[i]>2*arr[right] :
-            right+=1
-            count+= right-(mid+1)
-            
-    return count  
-def mergesort(arr, low, high):
-    count = 0
-    if low < high:
-        mid = (low + high) // 2
+        arr[i] = temp[i - low]
 
-        count += mergesort(arr, low, mid)
-        count += mergesort(arr, mid + 1, high)
-        count += countpair(arr, low, high, mid)
-        merge(arr, low, high, mid)
+def countPairs(arr, low, mid, high):
+    right = mid + 1
+    cnt = 0
+    for i in range(low, mid + 1):
+        while right <= high and arr[i] > 2 * arr[right]:
+            right += 1
+        cnt += (right - (mid + 1))
+    return cnt
 
-    return count
-def team(arr,n):
-    return mergesort(arr,0,n-1)
+def mergeSort(arr, low, high):
+    cnt = 0
+    if low >= high:
+        return cnt
+    mid = (low + high) // 2
+    cnt += mergeSort(arr, low, mid)  # left half
+    cnt += mergeSort(arr, mid + 1, high)  # right half
+    cnt += countPairs(arr, low, mid, high)  # Modification
+    merge(arr, low, mid, high)  # merging sorted halves
+    return cnt
 
-main()                  
-        
-                
-            
-        
+def team(skill: [int], n: int) -> int:
+    return mergeSort(skill, 0, n - 1)
+
+if __name__ == "__main__":
+    a = [4, 1, 2, 3, 1]
+    n = 5
+    cnt = team(a, n)
+    print("The number of reverse pair is:", cnt)
+
