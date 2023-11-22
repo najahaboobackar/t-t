e7acc6a825e5ed1f432da9cdf91d0c56d357bf47
najah <najahaboobacker79@gmail.com>
2023-11-22 19:51:21 +0530

    @

--- binarysearch/tofindx.py
+++ binarysearch/tofindx.py
@@ -1,20 +1,17 @@
 
-def binarySearch(nums: [int], target: int):
-    n = len(nums)  # size of the array
-    low = 0
-    high = n - 1
-
-    # Perform the steps:
-    while low <= high:
-        mid = (low + high) // 2
-        if nums[mid] == target:
+def binarySearch(arr,target):
+    n=len(arr)
+    low=0
+    high=n-1
+    mid=(low+high)//2
+    while low<=high:
+        if arr[mid]==target:
             return mid
-        elif target > nums[mid]:
-            low = mid + 1
+        elif target>arr[mid]:
+            low=mid+1
         else:
-            high = mid - 1
-    return -1
-
+            high=mid-1  
+    return -1       
 if __name__ == "__main__":
     a = [3, 4, 6, 7, 9, 12, 16, 17]
     target = 6
@@ -23,3 +20,6 @@ if __name__ == "__main__":
         print("The target is not present.")
     else:
         print("The target is at index:", ind)
+ 
+           
+        
