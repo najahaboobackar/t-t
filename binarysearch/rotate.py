ee98387ecd0fec92e8daa29a26686c0985497cd6
najah <najahaboobacker79@gmail.com>
2023-11-28 22:26:22 +0530

    @

--- binarysearch/rotate.py
+++ binarysearch/rotate.py
@@ -3,14 +3,24 @@ n=len(arr)
 low=0
 l=0
 k=4
+ans=0
 high=n-1
 while low<=high:
     mid=(low+high)//2
     if arr[mid]==k:
-        print(mid)
-    elif arr[low]>arr[mid]and arr[mid]>k:
-        high=mid-1
-    
+        ans=mid
+    elif arr[low]<=arr[mid]:
+        if arr[low]<=k and k<=arr[mid]:
+            high=mid-1
+        else:
+            low=mid+1
+    else:
+        if k>=arr[mid]and k <=arr[high]:
+            low=mid+1
+        else:
+            high=mid-1 
+print(ans)            
+                                                      
            
                
            
