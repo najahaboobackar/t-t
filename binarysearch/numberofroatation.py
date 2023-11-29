238478a792f0852d0769408037ddc5acfa0e2be3
najah <najahaboobacker79@gmail.com>
2023-11-29 12:49:18 +0530

    @

--- binarysearch/numberofroatation.py
+++ binarysearch/numberofroatation.py
@@ -1,27 +1,28 @@
-def numberofRotate(arr,n):
+def findKRotation(arr):
+    n=len(arr)
     low=0
     high=n-1
+    ans= float('inf')
     while low<=high:
-        mid=(low+high)//2
+        mid =(low+high)//2
         if arr[low]<=arr[high]:
             index=low
+            ans=arr[low]
+            break
         if arr[low]<=arr[mid]:
-            index=low
-            
+            if arr[low]<ans:
+                index=low
+                ans=arr[low]
             low=mid+1
-        if arr[mid]<=arr[high]:
-            index=mid
-            high=mid-1    
-        if arr[mid]>=arr[high] and arr[mid]>=arr[low]:
-            index=mid  
-    return index
-if __name__=="__main__":
-    arr = [3,4,5,1,2]
-    n=len(arr)
-    ans=numberofRotate(arr,n)
+        else:
+            if arr[mid]<=ans:
+                index=mid
+                ans=arr[mid]
+            high=mid-1
+    return index  
+if __name__ == "__main__":
+    arr = [4, 5, 6, 7, 0, 1, 2, 3]
+    ans = findKRotation(arr)
     print("The array is rotated", ans, "times.")
-          
-            
-        
-       
-                    
+
+                  
